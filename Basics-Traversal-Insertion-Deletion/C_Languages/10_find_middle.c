5475db97144cfca02019679584ba2aeb184f673f
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-22 11:26:42 +0200

    Create 10_find_middle.c

---
+++ Basics-Traversal-Insertion-Deletion/C_Languages/10_find_middle.c
@@ -0,0 +1,32 @@
+/* 10. Find the middle of a linked list (slow/fast pointer approach) */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* findMiddle(struct Node* head) {
+    struct Node* slow = head;
+    struct Node* fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+int main() {
+    struct Node* head = malloc(sizeof(struct Node));
+    head->data = 1;
+    head->next = malloc(sizeof(struct Node));
+    head->next->data = 2;
+    head->next->next = malloc(sizeof(struct Node));
+    head->next->next->data = 3;
+    head->next->next->next = NULL;
+
+    printf("Middle element: %d\n", findMiddle(head)->data);
+    return 0;
+}
