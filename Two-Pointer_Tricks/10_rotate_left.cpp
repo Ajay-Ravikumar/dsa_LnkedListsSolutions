fcd32c62c926eb4aa26627454b9a1c9477af5921
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-22 12:20:17 +0200

    Create 10_rotate_left.cpp

---
+++ Two-Pointer_Tricks/10_rotate_left.cpp
@@ -0,0 +1,24 @@
+/* 35. Rotate linked list to the left by k places */
+
+#include <iostream>
+using namespace std;
+
+struct Node{int data; Node* next; Node(int d):data(d),next(nullptr){}};
+
+Node* rotateLeft(Node* head,int k){
+    if(!head||!head->next||k==0) return head;
+    Node* cur=head; int len=1;
+    while(cur->next){ cur=cur->next; len++; }
+    cur->next=head; k%=len; int steps=k;
+    Node* newTail=head;
+    for(int i=1;i<steps;i++) newTail=newTail->next;
+    Node* newHead=newTail->next; newTail->next=nullptr;
+    return newHead;
+}
+
+void printList(Node* h){ while(h){ cout<<h->data<<" "; h=h->next;} cout<<endl; }
+
+int main(){
+    Node* h=new Node(1); h->next=new Node(2); h->next->next=new Node(3); h->next->next->next=new Node(4);
+    h=rotateLeft(h,2); printList(h);
+}
